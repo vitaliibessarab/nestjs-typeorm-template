4486bb3c4e176de4db4471042131cee05d3a6544
Vitalii Bessarab <vitalii.bessarab@gmail.com>
2024-03-12 14:33:40 +0200

    Provide config for database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.getOrThrow('MSSQL_HOST'),
+        port: +configService.getOrThrow('MSSQL_PORT'),
+        database: configService.getOrThrow('MSSQL_DATABASE'),
+        username: configService.getOrThrow('MSSQL_USERNAME'),
+        password: configService.getOrThrow('MSSQL_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: configService.getOrThrow('MSSQL_SYNCHRONIZE'),
+        options: { trustServerCertificate: true },
+        // options: { encrypt: false },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class DatabaseModule {}
