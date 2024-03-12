5a55adbb91c8d3a591d09df35d0cd606302d3421
Vitalii Bessarab <vitalii.bessarab@gmail.com>
2024-03-12 15:03:02 +0200

    Add User CRUD

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,14 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   username: string;
+
+  constructor(user: Partial<User>) {
+    Object.assign(this, user);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = new User(createUserDto);
+    await this.entityManager.save(user);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.userRepository.findOneBy({ id });
+    user.username = updateUserDto.username;
+    await this.entityManager.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return this.userRepository.delete(id);
   }
 }
