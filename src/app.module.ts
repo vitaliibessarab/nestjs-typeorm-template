575243e3422bb41a2b900fb35efabd417d9c938c
Vitalii Bessarab <vitalii.bessarab@gmail.com>
2024-03-12 14:46:23 +0200

    Run nest g resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
